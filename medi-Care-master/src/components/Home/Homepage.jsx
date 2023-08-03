7c86c1e30993a9b2af71a027a39379602b49c9dc
JanhaviG10 <139610838+JanhaviG10@users.noreply.github.com>
2023-08-03 20:48:09 +0530

    Initial commit

---
+++ medi-Care-master/src/components/Home/Homepage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+// components
+import HomePageBody from "./HomePageBody";
+// import HomePageHeader from "./HomePageHeader";
+import HomePageSlider from "./HomePageSlider";
+
+const Container = styled.div``;
+
+function HomePage() {
+  return (
+    <Container>
+      {/* <HomePageHeader /> */}
+      <HomePageSlider />
+      <HomePageBody />
+    </Container>
+  );
+}
+
+export default HomePage;
